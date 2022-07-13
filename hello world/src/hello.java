e46abab55d27f2b0f02354431395eceef51ef208
karthikvikram2000 <54365399+karthikvikram2000@users.noreply.github.com>
2022-07-13 11:56:35 +0530

    first-commit

---
+++ hello world/src/hello.java
@@ -0,0 +1,11 @@
+import java.util.*;  
+class hello {
+	public static void main(String args[]) {
+		Scanner sc= new Scanner(System.in);    //System.in is a standard input stream  
+		int a;
+		System.out.print("Enter first number- ");  
+		a=sc.nextInt();
+		System.out.println("number u have entered is"+a);
+		
+    }
+}
